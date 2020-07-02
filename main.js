5612ffa01e843133dd39f442a8e1656deee064a5
Gautier Miquet <xisabla.dev@gmail.com>
2020-07-02 22:11:56 +0200

    add dev scripts & prepare for bootstrap

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+const path = require('path')
 const { app, BrowserWindow } = require('electron')
+require('dotenv').config()
+
+if (process.env.RELOAD) {
+    const electron = path.join(__dirname, 'node_modules', '.bin', 'electron')
+
+    require('electron-reload')(__dirname, {
+        electron
+    })
+}
 
 function createWindow() {
     let win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 800,
         autoHideMenuBar: true,
         webPreferences: {
             nodeIntegration: true
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
 
     context: path.resolve(__dirname),
 
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
+    },
+
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/index.js'
