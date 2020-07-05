32ba9c7a89146e64a72abe9d0d8f3fad176037e6
Gautier Miquet <xisabla.dev@gmail.com>
2020-07-05 17:46:00 +0200

    remove useless stuff in index

--- src/index.tsx
+++ src/index.tsx
@@ -4,26 +4,4 @@ import { render } from 'react-dom'
 import { App } from './Components'
 import { store } from './Store'
 
-/**
-
-<App> --> contains global store and provide
-    <Sidebar>
-        <SidebarItem panelId="panelId" default?>Item</SidebarItem>
-    <Panels>
-        <Panel id="patcher">
-        <Panel id="patcher">
-        <Panel id="panelId" onShow?={} onHide?={} onToggle?={}>
-            <h1>Panel Content</h1>
-        </Panel>
-    </Panels>
-
- */
-
-// <SidebarItem panelId="patcher" name="Hello World">
-
-// Note: Those panels will be added on Panel JSX Element constructor
-store.panels.add('settings')
-store.panels.add('patcher')
-store.panels.add('test')
-
 render(<App store={store} />, document.getElementById('root'))
